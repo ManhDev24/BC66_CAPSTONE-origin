6de353b4866bc02a9fcf3e5b012d30da12ebce24
ManhDev24 <tienmanhpham2403@gmail.com>
2024-03-19 15:53:53 +0700

    update night mode v1

---
+++ index.js
@@ -0,0 +1,24 @@
+function darkMode() {
+  const navbar_input = document.getElementById("navbar-input");
+  navbar_input.classList.toggle("dark-mode");
+
+  const search_icon = document.querySelector(".search__icon");
+  search_icon.classList.toggle("dark-mode");
+
+  const dinnerContent = document.getElementById("dinner-content");
+  dinnerContent.classList.toggle("dark-mode");
+
+  const heading = dinnerContent.querySelector("h3");
+  heading.classList.toggle("text-white");
+
+  const darkMoonIcon = document.getElementById("dark-m");
+  const isDarkMode = darkMoonIcon.classList.contains("fa-sun");
+
+  if (isDarkMode) {
+    darkMoonIcon.classList.remove("fa-sun");
+    darkMoonIcon.classList.add("fa-moon");
+  } else {
+    darkMoonIcon.classList.remove("fa-moon");
+    darkMoonIcon.classList.add("fa-sun");
+  }
+}
